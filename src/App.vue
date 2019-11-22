097cb9bbd50e8f81ebef1e4cbefc33cbaeface8a
linxiaomiao <282504669@qq.com>
2019-11-22 17:05:34 +0800

    项目开发准备已完成！

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <header-all></header-all>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import HeaderAll from '@/pages/header/Header'
+export default {
+  name: 'App',
+  components: {
+    HeaderAll
+  }
+}
+</script>
+
+<style></style>
---
+++ src/pages/header/Header.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="header-all">
+    <div class="header-navList">
+      <i class="iconfont iconliebiao"></i>
+    </div>
+    <div class="header-logo">WINSTARS</div>
+    <div class="header-search">
+      <i class="iconfont iconsousuo"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+  import '@/assets/fonts/iconfont/iconfont.css'
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style lang="less" scoped>
+  .header-all{
+    display:flex;
+    height: 86px;
+    line-height: 86px;
+    background: #3b6caa;
+    .header-navList{
+      margin:0 20px;
+      color: #fff;
+      text-align: center;
+      i{
+        font-size:56px
+      }
+    }
+    .header-logo {
+      flex: 1;
+      color: #fff;
+      text-align: center
+    }
+    .header-search {
+      margin: 0 40px;
+      color: #fff;
+      text-align: center;
+      i{
+        font-size: 35px
+      }
+    }
+  }
+</style>
